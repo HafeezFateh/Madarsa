cb4371dff3479333223683fd7ea582a10d6a1fec
Hafeezur Rahman <hafeezofficial08@gmail.com>
2025-08-08 18:42:44 +0530

    V.1.1.7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,8 @@ function Header(){
         <>
             <div className="p-3 lg:pr-5 lg:m-5 bg-stone-50/10    border-stone-200 rounded-xl mt-5 mx-3  flex items-center justify-between">
                  <h1 className="text-xl text-white ml-2">Madarsa-e-Iqra</h1>
-                 <span onClick={toggleMenu} class="text-white material-symbols-outlined lg:hidden"><span className="lg:hidden">menu</span></span>
+                 <svg onClick={toggleMenu} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7 text-stone-50">
+                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" /></svg>
                  <div className="hidden lg:flex gap-5 text-stone-50">
                     <Link>About</Link>
                     <Link>Admission</Link>
